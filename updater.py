18d6855cbb576ed7be42c8310a801c659907b66c
Emilio Coppola <coppolaemilio@gmail.com>
2018-06-19 16:27:06 +0200

    updating data

--- updater.py
+++ updater.py
@@ -1,7 +1,11 @@
 import time
 import os
 
+iteration = 0
+
 while True:
   os.system('python weather.py')
   os.system('lazygit "updating data"')
-  time.sleep(60 * 30)
+  time.sleep(60 * 60)
+  iteration += 1
+  print('[+] ', str(iteration))
