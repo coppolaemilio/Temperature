0ab8607f22608a37e15315120f20366fd0f25663
Emilio Coppola <coppolaemilio@gmail.com>
2018-06-24 03:23:46 +0200

    making an agnostic update

--- updater.py
+++ updater.py
@@ -5,7 +5,9 @@ iteration = 0
 
 while True:
   os.system('python weather.py')
-  os.system('lazygit "updating data"')
+  os.system('git add .')
+  os.system('git commit -m "updating data"')
+  os.system('git push')
   time.sleep(60 * 60)
   iteration += 1
   print('[+] ', str(iteration))
